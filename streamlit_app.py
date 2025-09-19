e25b607c23be22b9edefa70eeaf9556f058733c0
jomarenriquez1211 <165797097+jomarenriquez1211@users.noreply.github.com>
2025-09-20 04:59:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,12 @@ from huggingface_hub import InferenceClient
 import streamlit as st
 
 hf_key = st.secrets["HF_API_KEY"]
-client = InferenceClient(token=hf_key)
+client = InferenceClient(model="google/flan-t5-base", token=hf_key)
 
 prompt = "Translate English to German: How are you?"
 
-response = client.text2text_generation(
-    model="google/flan-t5-base",
-    input=prompt,
-    max_new_tokens=300
+response = client.post(
+    json={"inputs": prompt, "parameters": {"max_new_tokens": 300}}
 )
 
-st.write(response)
+st.write(response[0]["generated_text"])
